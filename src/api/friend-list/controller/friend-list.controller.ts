40a91b8ffbfc4d9f03f98be314a9c6f693cb1cb2
yseonjin <yseonjin@gmail.com>
2022-08-20 23:48:08 +0900

    진행중

--- src/api/friend-list/controller/friend-list.controller.ts
+++ src/api/friend-list/controller/friend-list.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { FriendListService } from '../service/friend-list.service';
 
-@Controller()
+@Controller('friends')
 export class FriendListController {
   constructor(private readonly friendListService: FriendListService) {}
+
+  /**
+   * @description 보유한 친구의 목록을 조회
+   * -
+   */
+  @Get()
+  async getFriends() {}
 }
