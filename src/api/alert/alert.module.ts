7adc580433976c8952d0adc1c95767113bed582d
yseonjin <yseonjin@gmail.com>
2022-08-26 22:06:44 +0900

    fix: build

--- src/api/alert/alert.module.ts
+++ src/api/alert/alert.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuestionnaireListEntity } from 'src/api/questionnaire/model/questionnaire-list.entity';
-import { FriendListEntity } from 'src/api/friend/model/list/friend-list.entity';
 import { Member } from 'src/api/member/model/member.entity';
 import { AlertController } from './controller/alert.controller';
 import { AlertService } from './service/alert.service';
 import { AlertEntity } from './model/alert.entity';
+import { FriendEntity } from '../friend/model/friend.entity';
 
 @Module({
   controllers: [AlertController],
   imports: [
     TypeOrmModule.forFeature([
       QuestionnaireListEntity,
-      FriendListEntity,
+      FriendEntity,
       Member,
       AlertEntity,
     ]),
